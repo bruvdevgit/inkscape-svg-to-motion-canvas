54a866192cd4048dcefe22a95bb1c92e84d58f61
Haxwell <thehaxwell@proton.me>
2025-01-21 16:00:37 +0200

    changed output folder to dist and made typescript decleration file be produced by build

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from '@rollup/plugin-typescript';
 export default {
 	input: 'src/index.ts',
 	output: {
-		dir: 'output',
+		dir: 'dist',
 		format: 'cjs'
 	},
 	plugins: [typescript()]
