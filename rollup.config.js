ca2dddd7e71c1f8ef9e1385d87876c823b55d9c9
Haxwell <thehaxwell@proton.me>
2025-01-30 11:13:20 +0200

    setup executable script part

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,14 @@
 import typescript from '@rollup/plugin-typescript';
+import { preserveShebangs } from 'rollup-plugin-preserve-shebangs';
 
 export default {
-	input: 'src/index.ts',
+	input: ['src/index.ts', 'src/bin.ts'],
 	output: {
 		dir: 'dist',
 		format: 'cjs'
 	},
-	plugins: [typescript()]
+	plugins: [
+		typescript(),
+		preserveShebangs(),
+	]
 };
---
+++ src/bin.ts
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+console.log('in bin.js');
