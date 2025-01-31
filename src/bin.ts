0adeb125091e6818156e842e102d8cb12a275cbc
Haxwell <thehaxwell@proton.me>
2025-01-31 21:49:35 +0200

    try 2: fixed reading config file from caller directory

--- src/bin.ts
+++ src/bin.ts
@@ -7,7 +7,13 @@ import * as path from 'path';
 console.log('in bin.js');
 (async () => {
   const fs = createFsWrapper();
-  const callerPath = callerCallsite()?.getFileName();
+  let callerPath = callerCallsite()?.getFileName();
+  console.log(`callerPath: ${callerPath}`);
+  callerPath = callerCallsite({ depth: 1 })?.getFileName();
+  console.log(`callerPath{ depth: 1 }: ${callerPath}`);
+  callerPath = callerCallsite({ depth: 2 })?.getFileName();
+  console.log(`callerPath{ depth: 2 }: ${callerPath}`);
+
   if (callerPath == undefined) return;
   const callerDir = path.dirname(callerPath);
   const content = await fs.readFile(`${callerDir}/inkscapeSVGToMotionCanvasConfig.ts`);
