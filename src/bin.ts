a2b9dd4e9cf5b0d9f4f4a0ca7095eb8dae949768
Haxwell <thehaxwell@proton.me>
2025-01-31 23:03:04 +0200

    try 1: loading main config file

--- src/bin.ts
+++ src/bin.ts
@@ -3,12 +3,15 @@
 import { createFsWrapper } from "./FsWrapper";
 import * as path from 'path';
 import { cwd } from 'node:process';
+import { CodegenConfig } from ".";
+import { loadTsConfig } from 'config-file-ts';
 
 console.log('in bin.js');
 (async () => {
   const fs = createFsWrapper();
   let callerPath = cwd();
-  const content = await fs.readFile(`${callerPath}/inkscapeSVGToMotionCanvasConfig.ts`);
+  //const content = await fs.readFile(`${callerPath}/inkscapeSVGToMotionCanvas.config.ts`);
 
+  const content = loadTsConfig<CodegenConfig>("inkscapeSVGToMotionCanvas.config.ts");
   console.log('->>>> ', content);
 })();
