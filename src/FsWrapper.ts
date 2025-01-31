cc70532ce9e00cfd577e9466a27be29412258885
Haxwell <thehaxwell@proton.me>
2025-01-31 13:36:37 +0200

    added test of configuration file reading

---
+++ src/FsWrapper.ts
@@ -0,0 +1,56 @@
+import { Abortable } from 'node:events';
+import { OpenMode, PathLike } from 'node:fs';
+import * as fsImport from 'node:fs/promises';
+import * as path from 'path';
+
+export interface FsWrapper {
+  formatPathStrFromScriptLocation(p: PathLike): void;
+  writeFile(file: fsImport.FileHandle | PathLike,
+    data: string | Uint8Array): Promise<void>;
+
+  createDirIfNonExistant(dirName: string): Promise<void>;
+}
+
+class _FsWrapper {
+  constructor() {
+  }
+
+  prefixPathWithCallerPath(p: PathLike) {
+    return `${path.dirname(process.argv[1])}${p}`;
+  }
+
+  async createDirIfNonExistant(dirName: string) {
+    try {
+      // check if the dir exists
+      await fsImport.access(dirName, fsImport.constants.F_OK);
+    }
+    catch (err) {
+      // if the dir doesn't exist, create it
+      await fsImport.mkdir(dirName, { recursive: true });
+    }
+  }
+
+  async writeFile(file: fsImport.FileHandle | PathLike,
+    data: string | Uint8Array) {
+    await fsImport.writeFile(
+      file,
+      data);
+  }
+
+
+  async readFile(
+    path: PathLike | fsImport.FileHandle,
+    options?:
+      | ({
+        encoding?: null | undefined;
+        flag?: OpenMode | undefined;
+      } & Abortable)
+      | null,
+  ) {
+    await fsImport.readFile(path, options);
+  }
+}
+
+export function createFsWrapper() {
+  return new _FsWrapper();
+}
--- src/bin.ts
+++ src/bin.ts
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
+import { createFsWrapper } from "./FsWrapper";
+
 console.log('in bin.js');
+(async () => {
+  const fs = createFsWrapper();
+  const content = await fs.readFile(fs.prefixPathWithCallerPath('/inkscapeSVGToMotionCanvasConfig.ts'));
+  console.log(content);
+})();
