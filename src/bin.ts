1c45003503f60175521079c1816ae2e85f6f710c
Haxwell <thehaxwell@proton.me>
2025-01-31 22:02:09 +0200

    try3: fixed reading config file from caller directory

--- src/bin.ts
+++ src/bin.ts
@@ -1,18 +1,14 @@
 #!/usr/bin/env node
 
 import { createFsWrapper } from "./FsWrapper";
-import callerCallsite from 'caller-callsite';
 import * as path from 'path';
+import { cwd } from 'node:process';
 
 console.log('in bin.js');
 (async () => {
   const fs = createFsWrapper();
-  let callerPath = callerCallsite()?.getFileName();
+  let callerPath = cwd();
   console.log(`callerPath: ${callerPath}`);
-  callerPath = callerCallsite({ depth: 1 })?.getFileName();
-  console.log(`callerPath{ depth: 1 }: ${callerPath}`);
-  callerPath = callerCallsite({ depth: 2 })?.getFileName();
-  console.log(`callerPath{ depth: 2 }: ${callerPath}`);
 
   if (callerPath == undefined) return;
   const callerDir = path.dirname(callerPath);
