793fe4cd2c5374eedfd0cee794a9b394c87bfa19
Haxwell <thehaxwell@proton.me>
2025-02-02 18:31:31 +0200

    fixed main schema

--- src/mainConfigSchema.ts
+++ src/mainConfigSchema.ts
@@ -3,11 +3,17 @@ import myzod, { Infer } from 'myzod';
 const mainConfigSchema = myzod.object({
   inkscapeSVGs: myzod.array(myzod.object({
     input: myzod.object({
-      filePath: myzod.string().pattern(/^(.+)\/([^\/]+)\.svg$/gm),
+      filePath: myzod.string()
+        .withPredicate(value => /^(.+)\/([^\/]+)\.svg$/gm.test(value),
+          'string must be a valid path to an ".svg" file'),
     }),
     output: myzod.object({
-      viewAdderFunctionName: myzod.string().pattern(/^[$A-Za-z_]?[$0-9A-Za-z_]*$/gm),
-      directoryPath: myzod.string().pattern(/^(.+)\/([^\/]+)$/gm),
+      viewAdderFunctionName: myzod.string()
+        .withPredicate(value => /^[$A-Za-z_]?[$0-9A-Za-z_]*$/gm.test(value),
+          'string must be a valid function name'),
+      directoryPath: myzod.string()
+        .withPredicate(value => /^(.+)\/([^\/]+)$/gm.test(value),
+          'string must be a directory path'),
     }),
   })),
 });
