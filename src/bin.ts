f1a835bb48f5d01916adc032bd186c0cbc08e7c5
Haxwell <thehaxwell@proton.me>
2025-02-03 08:17:30 +0200

    generating mock output file try 1

--- src/bin.ts
+++ src/bin.ts
@@ -5,6 +5,7 @@ import { cwd } from 'node:process';
 import { createTOMLWrapper } from "./TOMLWrapper";
 import mainConfigSchema, { MainConfig } from "./mainConfigSchema";
 import chokidar from 'chokidar';
+import { makeDirectory } from 'make-dir';
 
 const log = console.log.bind(console);
 
@@ -22,12 +23,20 @@ console.log('in bin.js');
   //
 
   const inputFilePath = config.inkscapeSVGs[0].input.filePath;
+  const outputDirectoryPath = config.inkscapeSVGs[0].output.directoryPath;
+  const outputFilePath = `${outputDirectoryPath}/${config.inkscapeSVGs[0].output.viewAdderFunctionName}.jsx`;
 
   const watcher = chokidar.watch(inputFilePath, {
     persistent: true
   });
+
   watcher
-    .on('change', path => log(`File ${path} has been changed`))
+    .on('change', async path => {
+      log(`File ${path} has been changed`);
+      const inputFileContent = await fs.readFile(path);
+      await makeDirectory(outputDirectoryPath);
+      await fs.writeFile(outputFilePath, inputFileContent);
+    });
 
 
 })();
